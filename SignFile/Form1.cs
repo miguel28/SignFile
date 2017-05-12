0a146362cc351ab60fc9814dafce824e00ea30f9
Miguel Silva <miguelinho_28@hotmail.com>
2017-05-11 22:18:54 -0600

    First Commit

---
+++ SignFile/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace SignFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            byte[] buffer = new byte[] { 0x00, 0x00, 0x00 };
+
+            RSACryptoServiceProvider csp = RSASignatures.ReadRSAPrivateKeyPem("private.pem");
+            string signature = RSASignatures.Sign(buffer , csp);
+
+            buffer = new byte[] { 0x00, 0x00, 0x01 };
+
+            RSACryptoServiceProvider cspDec = RSASignatures.ReadRSAPublicKeyPem("public.pem");
+            bool a = RSASignatures.Verify(buffer, signature, cspDec);
+        }
+    }
+}
---
+++ SignFile/RSASignatures.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace SignFile
+{
+    public class RSASignatures
+    {
+        public static string Sign(byte[] data, RSACryptoServiceProvider csp)
+        {
+            SHA1Managed sha1 = new SHA1Managed();
+            byte[] hash = sha1.ComputeHash(data);
+            byte[] signature_bytes = csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
+
+            return Convert.ToBase64String(signature_bytes);
+        }
+
+        public static bool Verify(byte[] data, string signature, RSACryptoServiceProvider csp)
+        {
+            // Hash the data
+            SHA1Managed sha1 = new SHA1Managed();
+            byte[] hash = sha1.ComputeHash(data);
+            byte[] signature_bytes = Convert.FromBase64String(signature);
+
+            // Verify the signature with the hash
+            return csp.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA1"), signature_bytes);
+        }
+
+        public static RSACryptoServiceProvider ReadRSAPrivateKeyPem(string filename)
+        {
+            string pem = System.IO.File.ReadAllText(filename);
+            byte[] certBuffer = GetBytesFromPEM(pem, "RSA PRIVATE KEY");
+            return CreateRsaProviderFromPrivateKey(certBuffer);
+        }
+
+        public static RSACryptoServiceProvider ReadRSAPublicKeyPem(string filename)
+        {
+            string pem = System.IO.File.ReadAllText(filename);
+            byte[] certBuffer = GetBytesFromPEM(pem, "PUBLIC KEY");
+            return DecodeX509PublicKey(certBuffer);
+        }
+
+        private static byte[] GetBytesFromPEM(string pemString, string section)
+        {
+            var header = String.Format("-----BEGIN {0}-----", section);
+            var footer = String.Format("-----END {0}-----", section);
+
+            var start = pemString.IndexOf(header, StringComparison.Ordinal);
+            if (start < 0)
+                return null;
+
+            start += header.Length;
+            var end = pemString.IndexOf(footer, start, StringComparison.Ordinal) - start;
+
+            if (end < 0)
+                return null;
+
+            return Convert.FromBase64String(pemString.Substring(start, end).Replace("\n", ""));
+        }
+
+        private static RSACryptoServiceProvider CreateRsaProviderFromPrivateKey(byte[] privateKey)
+        {
+            var RSA = new RSACryptoServiceProvider();
+            var RSAparams = new RSAParameters();
+
+            using (BinaryReader binr = new BinaryReader(new MemoryStream(privateKey)))
+            {
+                byte bt = 0;
+                ushort twobytes = 0;
+                twobytes = binr.ReadUInt16();
+                if (twobytes == 0x8130)
+                    binr.ReadByte();
+                else if (twobytes == 0x8230)
+                    binr.ReadInt16();
+                else
+                    throw new Exception("Unexpected value read binr.ReadUInt16()");
+
+                twobytes = binr.ReadUInt16();
+                if (twobytes != 0x0102)
+                    throw new Exception("Unexpected version");
+
+                bt = binr.ReadByte();
+                if (bt != 0x00)
+                    throw new Exception("Unexpected value read binr.ReadByte()");
+
+                RSAparams.Modulus = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.Exponent = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.D = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.P = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.Q = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.DP = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.DQ = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.InverseQ = binr.ReadBytes(GetIntegerSize(binr));
+            }
+
+            RSA.ImportParameters(RSAparams);
+            return RSA;
+        }
+
+        private static int GetIntegerSize(BinaryReader binr)
+        {
+            byte bt = 0;
+            byte lowbyte = 0x00;
+            byte highbyte = 0x00;
+            int count = 0;
+            bt = binr.ReadByte();
+            if (bt != 0x02)
+                return 0;
+            bt = binr.ReadByte();
+
+            if (bt == 0x81)
+                count = binr.ReadByte();
+            else
+                if (bt == 0x82)
+            {
+                highbyte = binr.ReadByte();
+                lowbyte = binr.ReadByte();
+                byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
+                count = BitConverter.ToInt32(modint, 0);
+            }
+            else
+            {
+                count = bt;
+            }
+
+            while (binr.ReadByte() == 0x00)
+            {
+                count -= 1;
+            }
+            binr.BaseStream.Seek(-1, SeekOrigin.Current);
+            return count;
+        }
+
+
+
+
+
+
+        public static RSACryptoServiceProvider DecodeX509PublicKey(byte[] x509key)
+        {
+            byte[] SeqOID = { 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01 };
+
+            MemoryStream ms = new MemoryStream(x509key);
+            BinaryReader reader = new BinaryReader(ms);
+
+            if (reader.ReadByte() == 0x30)
+                ReadASNLength(reader); //skip the size
+            else
+                return null;
+
+            int identifierSize = 0; //total length of Object Identifier section
+            if (reader.ReadByte() == 0x30)
+                identifierSize = ReadASNLength(reader);
+            else
+                return null;
+
+            if (reader.ReadByte() == 0x06) //is the next element an object identifier?
+            {
+                int oidLength = ReadASNLength(reader);
+                byte[] oidBytes = new byte[oidLength];
+                reader.Read(oidBytes, 0, oidBytes.Length);
+                if (oidBytes.SequenceEqual(SeqOID) == false) //is the object identifier rsaEncryption PKCS#1?
+                    return null;
+
+                int remainingBytes = identifierSize - 2 - oidBytes.Length;
+                reader.ReadBytes(remainingBytes);
+            }
+
+            if (reader.ReadByte() == 0x03) //is the next element a bit string?
+            {
+                ReadASNLength(reader); //skip the size
+                reader.ReadByte(); //skip unused bits indicator
+                if (reader.ReadByte() == 0x30)
+                {
+                    ReadASNLength(reader); //skip the size
+                    if (reader.ReadByte() == 0x02) //is it an integer?
+                    {
+                        int modulusSize = ReadASNLength(reader);
+                        byte[] modulus = new byte[modulusSize];
+                        reader.Read(modulus, 0, modulus.Length);
+                        if (modulus[0] == 0x00) //strip off the first byte if it's 0
+                        {
+                            byte[] tempModulus = new byte[modulus.Length - 1];
+                            Array.Copy(modulus, 1, tempModulus, 0, modulus.Length - 1);
+                            modulus = tempModulus;
+                        }
+
+                        if (reader.ReadByte() == 0x02) //is it an integer?
+                        {
+                            int exponentSize = ReadASNLength(reader);
+                            byte[] exponent = new byte[exponentSize];
+                            reader.Read(exponent, 0, exponent.Length);
+
+                            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+                            RSAParameters RSAKeyInfo = new RSAParameters();
+                            RSAKeyInfo.Modulus = modulus;
+                            RSAKeyInfo.Exponent = exponent;
+                            RSA.ImportParameters(RSAKeyInfo);
+                            return RSA;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int ReadASNLength(BinaryReader reader)
+        {
+            //Note: this method only reads lengths up to 4 bytes long as
+            //this is satisfactory for the majority of situations.
+            int length = reader.ReadByte();
+            if ((length & 0x00000080) == 0x00000080) //is the length greater than 1 byte
+            {
+                int count = length & 0x0000000f;
+                byte[] lengthBytes = new byte[4];
+                reader.Read(lengthBytes, 4 - count, count);
+                Array.Reverse(lengthBytes); //
+                length = BitConverter.ToInt32(lengthBytes, 0);
+            }
+            return length;
+        }
+    }
+}
