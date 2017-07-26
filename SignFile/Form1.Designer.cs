21dbfb08b7eec1d4bd05dd20303cbee867506204
Miguel Silva <miguelinho_28@hotmail.com>
2017-07-25 18:49:46 -0600

    Encrypt String Example added

--- SignFile/Form1.Designer.cs
+++ SignFile/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnSign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(34, 93);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(130, 38);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnSign
+            // 
+            this.btnSign.Location = new System.Drawing.Point(34, 32);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(130, 38);
+            this.btnSign.TabIndex = 1;
+            this.btnSign.Text = "Sign";
+            this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnSign);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnSign;
     }
 }
 
--- SignFile/Form1.cs
+++ SignFile/Form1.cs
@@ -20,15 +20,36 @@ namespace SignFile
         {
             InitializeComponent();
 
+            
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
             byte[] buffer = new byte[] { 0x00, 0x00, 0x00 };
 
             RSACryptoServiceProvider csp = RSASignatures.ReadRSAPrivateKeyPem("private.pem");
-            string signature = RSASignatures.Sign(buffer , csp);
+            string signature = RSASignatures.Sign(buffer, csp);
 
             buffer = new byte[] { 0x00, 0x00, 0x01 };
 
             RSACryptoServiceProvider cspDec = RSASignatures.ReadRSAPublicKeyPem("public.pem");
             bool a = RSASignatures.Verify(buffer, signature, cspDec);
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider cspDec = RSASignatures.ReadRSAPublicKeyPem("public.pem");
+            string text_to_sig = "PASSWORD";
+            byte[] buffer = Encoding.UTF8.GetBytes(text_to_sig);
+
+            byte[] encripted = cspDec.Encrypt(buffer, false);
+            Console.WriteLine(Convert.ToBase64String(encripted));
+
+            RSACryptoServiceProvider csp = RSASignatures.ReadRSAPrivateKeyPem("private.pem");
+            byte[] dec = csp.Decrypt(encripted, false);
+            string decstr = Encoding.UTF8.GetString(dec);
+
+            Console.WriteLine(decstr);
         }
     }
 }
